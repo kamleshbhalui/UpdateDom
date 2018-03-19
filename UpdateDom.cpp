b268df5c43690cdf57a68c06b9986ac2b8a8c565
Kamlesh Kumar <kamleshbhalui@gmail.com>
2018-03-19 14:23:57 +0530

    Update UpdateDom.cpp

--- UpdateDom.cpp
+++ UpdateDom.cpp
@@ -1,1 +1,13 @@
+/// This is a single class which can be used for updating dominator tree.
+/// It stores the dominator tree and define update policy.
+/// Whenever a transformation pass makes changes to the CFG. It only need to sends a indication to
+/// the updater class and then this makes update to the dom tree.
+/// If some one needed the post dominator tree then we can construct it from dominator tree.
 
+template<typename UpdateDom>
+class UpdateDom{
+
+public:
+    Update
+
+};
